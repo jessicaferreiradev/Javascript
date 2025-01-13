7b1b16536d5def1be9cb72145261c0f7e0baba2c
Jéssica Barbosa <jessicaffff11@gmail.com>
2025-01-13 10:46:52 -0300

    validação de formulário
    exercício que valida nome e e-mail.

--- exercicios/exerciciosgpt/validacao-de-formulario/script.js
+++ exercicios/exerciciosgpt/validacao-de-formulario/script.js
@@ -1,4 +1,18 @@
-function clicar(alerta) {
-    alert(alerta); // Exibe o valor do parâmetro
-}
+function confirmar(){
+    var nome = document.getElementById('txtnome');
+    var mail = document.getElementById('txtemail');
+    var res = document.getElementById('resultado')
+    const email = mail.value
+    if (nome.value.trim() === "" || mail.value.trim() === ""){
+        alert('Insira as informações corretamente');
+    } else {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (regex.test(email)) {
+            alert('E-mail válido!');
+            res.innerHTML = 'Cadastro realizado com sucesso!'
+        } else {
+            alert('Insira seu e-mail corretamente!')
+        }
+    }
 
+}
