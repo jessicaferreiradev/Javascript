cbf6b378c3558d91d3c4452189628e963ecdb394
Jéssica Barbosa <jessicaffff11@gmail.com>
2025-01-13 14:39:23 -0300

    esconder texto

---
+++ exercicios/exerciciosgpt/esconder-texto/script.js
@@ -0,0 +1,8 @@
+function clicar(){
+    document.getElementById('escondertexto').style.display = 'none';
+    document.getElementById('escondertitulo').style.display = 'none';
+}
+function mostrar(){
+    document.getElementById('escondertexto').style.display = 'block';
+    document.getElementById('escondertitulo').style.display = 'block';
+}
