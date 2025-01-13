770404242685aa3fd29882fb700901409ab89858
Jéssica Barbosa <jessicaffff11@gmail.com>
2025-01-13 14:05:29 -0300

    mudar-imagem

---
+++ exercicios/exerciciosgpt/mudar-imagem/script.js
@@ -0,0 +1,12 @@
+function entrar(){
+    var ft = document.getElementById('foto');
+    ft.src = 'rose.jpg'
+}
+function sair(){
+    var ft = document.getElementById('foto');
+    ft.src = 'garden.jpg'
+}
+function clicar(){
+    var ft = document.getElementById('foto');
+    ft.src = 'jardim.jpg'
+}
